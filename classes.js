310e23bcc9b87ea1b07dbc9e7d4116ae9039dca5
diksha0562 <30476228+diksha0562@users.noreply.github.com>
2017-10-31 23:38:35 +0530

    Add files via upload

--- classes.js
+++ classes.js
@@ -9,6 +9,19 @@ class Animal{
         console.log(`hi, i m ${this.name}, my color is ${this.color}`);
     }
 }
-let lion=new Animal("abc","golden");
-console.log(lion);
-lion.speak();
+//let lion=new Animal("abc","golden");
+//console.log(lion);
+//lion.speak();
+class Lion extends Animal{
+    constructor(name,color,role,home){
+        super(name,color);
+        this.role=role;
+        this.home=home;
+    }
+    roar(){
+        console.log(`I m ${this.role}, I lives in ${this.home}`);
+    }
+}
+let lion= new Lion("abc","golden","king","rocks");
+//console.log(lion);
+lion.roar();
