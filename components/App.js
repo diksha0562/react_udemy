cb18f28200ff3aacc6462769d093e81ca474ef07
diksha0562 <30476228+diksha0562@users.noreply.github.com>
2017-11-01 12:14:36 +0530

    Add files via upload

--- components/App.js
+++ components/App.js
@@ -2,11 +2,23 @@ import React,{Component} from 'react';
 //component is not actual defaul for react library
 
 //create class app which extends component from react
+import {Form, FormControl,Button} from 'react-bootstrap';
 class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            text: ''//update text
+        }
+    }
 render(){
     return(
         <div>
             <h2> Note to Self</h2>
+            <Form inline>
+                <FormControl onChange={event=>this.setState({text: event.target.value})}/>
+                {' '}
+                <Button onClick={()=>console.log(this.state)}>Submit </Button>
+                </Form>
             </div>
     )
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App,{color, number} from './components/App';
+import './index.css';
 //rendering jsx onto DOM
 ReactDOM.render(<App/>,document.getElementById("root"));
 console.log(color,number);
